68a4bab80bd1433c4e8ff35553f2717a16e44466
mauricio-dddelc <mauriciod73@gmail.com>
2019-08-17 17:05:38 +0200

    Program placeholder

---
+++ src/RoverConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoverConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
